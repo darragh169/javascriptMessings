5bdd44c8b470ed95f058cc37e4d74aab33591a32
Darragh Bourke <darragh169@gmail.com>
2020-11-17 15:02:09 -0800

    Update removeDuplicates.js

--- removeDuplicates.js
+++ removeDuplicates.js
@@ -1,8 +1,20 @@
 const str = 'this is is a test test string'
 
-function removeDuplicates (str = '') {
+function removeDuplicatesWithSet (str = '') {
   let tempSet = new Set(str.split(' '))
   return [...tempSet].join(' ')
 }
 
-console.log(removeDuplicates(str))
+function removeDuplicatesWithoutSet (str = '') {
+  const arr = []
+  const strArr = str.split(' ')
+  for (let idx = 0; idx < strArr.length; idx++) {
+    if(!arr.includes(strArr[idx])) {
+      arr.push(strArr[idx])
+    }
+  }
+  return arr.join(' ')
+}
+
+console.log(removeDuplicatesWithSet(str))
+console.log(removeDuplicatesWithoutSet(str))
